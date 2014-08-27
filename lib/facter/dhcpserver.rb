cc3b072068a19c038f2c946194a6bc26f2e3f2fe
Stanislav Makar <stas.makar@gmail.com>
2014-08-27 17:15:41 +0300

    added the possibility to find out the dhcp server for all interface

--- lib/facter/dhcpserver.rb
+++ lib/facter/dhcpserver.rb
@@ -1,20 +1,24 @@
 require 'facter'
+require 'facter/util/ip'
 
-dhcp_eth0_leases_file = File.open('/var/lib/dhcp/dhclient.eth0.leases','r')
-result = Array.new
-dhcp_eth0_leases_file.each_line do | line |
-  if line.include? "dhcp-server-identifier"
-      result.push(line.scan /\d+\.\d+\.\d+\.\d+/)
-  end
-end 
-dhcp_eth0_leases_file.close
-
-#puts result.last
-
-Facter.add('dhcpserver') do
+Facter::Util::IP.get_interfaces.each do |interface|
+ if interface == 'lo'
+    next
+ end
+ if !File.exist?("/var/lib/dhcp/dhclient.#{interface}.leases")
+    next
+ end
+  Facter.add('dhcpserver' + '_' + Facter::Util::IP.alphafy(interface)) do
     setcode do
+       dhcp_leases_file = File.open("/var/lib/dhcp/dhclient.#{interface}.leases",'r')
+       result = Array.new
+       dhcp_leases_file.each_line do | line |
+          if line.include? "dhcp-server-identifier"
+             result.push(line.scan /\d+\.\d+\.\d+\.\d+/)
+          end
+       end
+       dhcp_leases_file.close
        result.last
-#       puts result.last
     end
+   end
 end
-

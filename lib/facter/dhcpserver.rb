7e31338f86b940d9b224e392e96605fe663c109a
Stas Makar <stas.makar@gmail.com>
2014-08-31 12:09:45 +0300

    performed refactoring

--- lib/facter/dhcpserver.rb
+++ lib/facter/dhcpserver.rb
@@ -2,23 +2,17 @@ require 'facter'
 require 'facter/util/ip'
 
 Facter::Util::IP.get_interfaces.each do |interface|
- if interface == 'lo'
-    next
- end
- if !File.exist?("/var/lib/dhcp/dhclient.#{interface}.leases")
-    next
- end
+  next if interface == 'lo'
+  next if !File.exist?("/var/lib/dhcp/dhclient.#{interface}.leases")
   Facter.add('dhcpserver' + '_' + Facter::Util::IP.alphafy(interface)) do
     setcode do
-       dhcp_leases_file = File.open("/var/lib/dhcp/dhclient.#{interface}.leases",'r')
-       result = Array.new
-       dhcp_leases_file.each_line do | line |
-          if line.include? "dhcp-server-identifier"
-             result.push(line.scan /\d+\.\d+\.\d+\.\d+/)
-          end
-       end
-       dhcp_leases_file.close
-       result.last
+      dhcp_leases_file = File.open("/var/lib/dhcp/dhclient.#{interface}.leases",'r')
+      result = Array.new
+      dhcp_leases_file.each_line do | line |
+        result.push(line.scan /\d+\.\d+\.\d+\.\d+/) if line.include? "dhcp-server-identifier"
+      end
+      dhcp_leases_file.close
+      result.last
     end
-   end
+  end
 end
